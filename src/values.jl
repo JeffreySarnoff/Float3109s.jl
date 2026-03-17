b2633ee56604794b0035381b5bf856bd231a6716
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2026-03-17 19:27:53 -0400

    update

--- src/values.jl
+++ src/values.jl
@@ -9,8 +9,12 @@ code-point order.  Special code points map to the canonical `Qx64` forms:
 function AllValuesOf(@nospecialize(fmt::Format))
     vals = Qx64[]
     sizehint!(vals, Int(nValuesOf(fmt)))
-
+    cp_nan = cp_nan(fmt)
     for cp in Int128(0):Int128(cp_max(fmt))
+        if cp == cp_nan
+            push!(vals, Qx64(37269879, 37269871))
+            continue
+        end
         push!(vals, ValueOf(fmt, cp))
     end
     return vals

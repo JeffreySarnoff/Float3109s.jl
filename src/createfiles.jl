f91a30c15c44be39cf14d44d0000fd4cb42cc933
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2026-03-17 20:52:41 -0400

    up

--- src/createfiles.jl
+++ src/createfiles.jl
@@ -13,7 +13,7 @@ end
 for K in MinK:MaxK
     subdir = string("K", K)
     subpath = aspath(HexStringsBase, subdir)
-    for P in 1:K
+    for P in 1:K-1
         subsubdir = string("P", P)
         subsubpath = aspath(subpath, subsubdir)
         if !isdir(subsubpath)
@@ -26,6 +26,25 @@ for K in MinK:MaxK
         fourpaths = (; uf=aspath(subsubpath, fournames.uf), ue=aspath(subsubpath, fournames.ue),
             sf=aspath(subsubpath, fournames.sf), se=aspath(subsubpath, fournames.se))
         for path in values(fourpaths)
+            if !isfile(path)
+                @info "Creating file $path"
+                open(path, "w") do io
+                    write(io, "")
+                end
+            end
+        end
+    end
+    for P in K:K
+        subsubdir = string("P", P)
+        subsubpath = aspath(subpath, subsubdir)
+        if !isdir(subsubpath)
+            @info "Creating directory $subsubpath"
+            mkpath(subsubpath)
+        end
+        basename = string("Binary", K, "p", P)
+        twonames = (; uf=string(basename, "uf", Ext), ue=string(basename, "ue", Ext))
+        twopaths = (; uf=aspath(subsubpath, twonames.uf), ue=aspath(subsubpath, twonames.ue))
+        for path in values(twopaths)
             if !isfile(path)
                 @info "Creating file $path"
                 open(path, "w") do io
